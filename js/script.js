0886e8ee48939ed75a11e2b157663ef7022be6f5
DmitriyRodkin <122818500+DmitriyRodkin@users.noreply.github.com>
2023-02-14 13:47:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const contactButton = document.querySelector(".asdasdsa")
-const sendText = document.querySelector(".section-registration")
-const sendParagraph = document.querySelector(".registration")
-contactButton.onclick = function(){
-    sendText.classList.toggle("dddddddddd")
-    sendParagraph.classList.toggle("dddddddddd")
+const ButtonRegisrt = document.querySelector(".button-click")
+ButtonRegisrt.onclick = function(){
+    ButtonRegisrt.classList.toggle("button-click")
 }
 
     
